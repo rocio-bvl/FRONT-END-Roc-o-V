838f8cb1e21d503773c6183a0fa49c65ae0805d5
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2026-05-04 16:25:30 -0400

    avance login y register

---
+++ script.js
@@ -0,0 +1,55 @@
+// Se captura el formulario
+const form = document.getElementById("formulario");
+const mensaje = document.getElementById("mensaje");
+
+// Detecta cuando el usuario hace submit
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const correo = document.getElementById("correo").value;
+    const password = document.getElementById("password").value;
+
+    // Validación
+    if (correo === "" || password === "") {
+        mensaje.textContent = "Todos los campos son obligatorios";
+        return;
+    }
+    if (!esEmailValido(correo)) {
+        mensaje.textContent = "El formato del correo es incorrecto.";
+        return;
+    }
+
+    // Llama al fetch()
+    login(correo, password);
+});
+
+function esEmailValido(correo) {let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+return regex.test(email);}
+
+// Fetch
+async function login(correo, password) {
+
+    try {
+        const respuesta = await fetch("http://localhost:3000/api/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                correo,
+                password
+            })
+        });
+
+        const data = await respuesta.json();
+
+        if (respuesta.ok) {
+            mensaje.textContent = "Login exitoso";
+        } else {
+            mensaje.textContent = data.error;
+        }
+
+    } catch (error) {
+        mensaje.textContent = "Error de conexión";
+    }
+}
