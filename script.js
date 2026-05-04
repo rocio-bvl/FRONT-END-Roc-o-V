41ce80f5e853564b530665592b434c7856bd0a58
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2026-05-04 16:44:14 -0400

    avance login y register

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const mensaje = document.getElementById("mensaje");
 // Detecta cuando el usuario hace submit
 form.addEventListener("submit", async function (e) {
     e.preventDefault();
-
+console.log("Hola")
     const correo = document.getElementById("correo").value;
     const password = document.getElementById("password").value;
 
@@ -14,6 +14,8 @@ form.addEventListener("submit", async function (e) {
         mensaje.textContent = "Todos los campos son obligatorios";
         return;
     }
+    console.log("Hola2222",correo)
+
     if (!esEmailValido(correo)) {
         mensaje.textContent = "El formato del correo es incorrecto.";
         return;
@@ -24,7 +26,7 @@ form.addEventListener("submit", async function (e) {
 });
 
 function esEmailValido(correo) {let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-return regex.test(email);}
+return regex.test(correo);}
 
 // Fetch
 async function login(correo, password) {
@@ -36,17 +38,21 @@ async function login(correo, password) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                correo,
+                email: correo,
                 password
             })
         });
 
         const data = await respuesta.json();
+    console.log("Hola2222",respuesta.ok,respuesta)
 
         if (respuesta.ok) {
             mensaje.textContent = "Login exitoso";
+
+
+            
         } else {
-            mensaje.textContent = data.error;
+            mensaje.textContent =  "hola" //data.error;
         }
 
     } catch (error) {
